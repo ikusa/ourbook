6e2f6b8303834a5298ae8aa9965e9271a8f2b84b
Joshua <joshuasetiawan04@gmail.com>
2018-11-15 16:33:15 +0700

    Updates

---
+++ precache-manifest.4c5e9ddba6f28ab41e63bdc9a61e571e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00ad1ecfc942cec483e",
+    "url": "/ourbook/static/js/runtime~main.e00ad1ec.js"
+  },
+  {
+    "revision": "4fc5579a7f181af7e94d",
+    "url": "/ourbook/static/js/main.4fc5579a.chunk.js"
+  },
+  {
+    "revision": "26d9beaaa5d7d2d313ef",
+    "url": "/ourbook/static/js/1.26d9beaa.chunk.js"
+  },
+  {
+    "revision": "4fc5579a7f181af7e94d",
+    "url": "/ourbook/static/css/main.aa60a5df.chunk.css"
+  },
+  {
+    "revision": "74d0f3806b373ae89ef46cd3a067431c",
+    "url": "/ourbook/index.html"
+  }
+];
